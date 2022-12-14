51b15febceb6e258e4b732b6a9c1413914fb495c
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-12-14 19:22:37 -0300

    alterando package para não rodar npn

--- config/specs.conf.js
+++ config/specs.conf.js
@@ -7,7 +7,8 @@ let specsConf = process.env.PLATFORM == 'android' ? {
 } : {
     specs: [
         './test/specs/products.spec.js',
-        './test/specs/search.spec.js'
+        './test/specs/search.spec.js',
+        './test/specs/productCart.spec.js'
     ]
 }
 
